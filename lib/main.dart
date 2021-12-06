6a350b31e82b9379d30d8ef10f2c258b2fe88a7b
composer6251 <dfennell31@gmail.com>
2021-12-06 15:47:15 -0500

    Adding basic scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp()
-    // const MaterialApp(
-    //   home: Center(
-    //     child: Text('Hello WOrld'),
-    //   ),
-    // ),
+  runApp(
+    MaterialApp( //Material App has home property which will have a Scaffold widget in it's place
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Image(
+          image: NetworkImage("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"),
+        ),
+      ),
+    ),
   );
 }
 
