8b8f4e71aa93860ec22ec64bf20f43aab6ba824e
composer6251 <dfennell31@gmail.com>
2021-12-09 13:51:15 -0500

    Messing with UI elements

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ void main() {
   );
 }
 
+//class MyApp extends
+
+
 class MyApp extends StatelessWidget {
  // const MyApp({Key? key}) : super(key: key);
 
@@ -16,11 +19,16 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blue,
         body: SafeArea(
           child: Column(
+            mainAxisSize: MainAxisSize.max,// Column Height max, or min (only what's needed to fit children)
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, // = Y axis for column, X axis for row.  align children within Column (similar to CSS flexbox)
+            crossAxisAlignment: CrossAxisAlignment.stretch, // opposite direction of mainAxisALignment.
+            verticalDirection: VerticalDirection.up, //  Direction of children
+
             children: [
               Container(
                 child: Text('Container 1'),
                 height: 100.0,
-                width: 100.0,
+                width: 400.0,
                 color: Colors.white,
                 margin: EdgeInsets.fromLTRB(30,20,10,0),
                 padding: EdgeInsets.all(20),
@@ -32,13 +40,28 @@ class MyApp extends StatelessWidget {
                 color: Colors.red,
                 margin: EdgeInsets.fromLTRB(30,20,10,0),
                 padding: EdgeInsets.all(20),
-              )
+              ),
+              // SizedBox( // to create space between children.
+              //   height: 100,
+              // ),
+              Container(
+                child: Text('Container 3'),
+                height: 100,
+                width: 100 ,
+                color: Colors.green,
+                margin: EdgeInsets.fromLTRB(30,20,10,0),
+                padding: EdgeInsets.all(20),
+              ),
+              Container(
+                width: double.infinity, // as wide as the screen allows
+              ),
             ],
           )
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.red,
-          child: Icon(Icons.add_shopping_cart_sharp),
+        //  child: Icon(Icons.add_shopping_cart_sharp),
+          child: Text('A'),
         ),
       ),
     );
