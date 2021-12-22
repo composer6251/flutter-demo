56b2bf8578f2f24460be4f62ffc705c97d493d9e
composer6251 <dfennell31@gmail.com>
2021-12-22 13:39:49 -0500

    Created constraints experiment branch. Working on fretboard/staff with Dividers.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ void main() {
 
 //class MyApp extends
 
+/******WIDGET CHARACTERISTICS*******/
+//1. Column:
 
 class MyApp extends StatelessWidget {
  // const MyApp({Key? key}) : super(key: key);
@@ -15,74 +17,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: SafeArea(
+        home: Container(
+          padding: EdgeInsets.all(50.0),
+          color: Colors.blue,
           child: Column(
-            mainAxisSize: MainAxisSize.max,// Column Height max, or min (only what's needed to fit children)
-            mainAxisAlignment: MainAxisAlignment.spaceBetween, // = Y axis for column, X axis for row.  align children within Column (similar to CSS flexbox)
-            crossAxisAlignment: CrossAxisAlignment.stretch, // opposite direction of mainAxisALignment.
-            verticalDirection: VerticalDirection.up, //  Direction of children
-
             children: [
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                      'CARD 1',
-                       style: TextStyle(
-                         fontSize: 20.0,
-                  )),
-                ),
-                color: Colors.white,
-                margin: EdgeInsets.fromLTRB(30,20,10,0),
-              ),
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/TestingLogo.png'),
-
-              ),
-              // SizedBox( // to create space between children.
-              //   height: 100,
-              // ),
               Container(
-                child: Text('Container 3'),
-                height: 30,
-                width: 100 ,
-                color: Colors.green,
-                margin: EdgeInsets.fromLTRB(30,20,10,0),
-                padding: EdgeInsets.all(20),
-              ),
-              Container(
-                width: double.infinity, // as wide as the screen allows
-              ),
-              Container(
-                child: Row(
+                color: Colors.black26,
+                child: Column(
                   children: [
-                    Icon(
-                      Icons.add_shopping_cart,
-                      size: 100.0,
-                      color: Colors.lightBlueAccent,
+                    const Divider(
+                      height: 40,
+                      thickness: 5,
+                      indent: 5,
+                      color: Colors.black,
+                    ),
+                    const Divider(
+                      height: 40,
+                      thickness: 5,
+                      indent: 5,
+                      color: Colors.black,
+                    ),
+                    const Divider(
+                      height: 40,
+                      thickness: 5,
+                      indent: 5,
+                      color: Colors.black,
+                    ),
+                    const Divider(
+                      height: 40,
+                      thickness: 5,
+                      indent: 5,
+                      color: Colors.black,
+                    ),
+                    const Divider(
+                      height: 40,
+                      thickness: 5,
+                      indent: 5,
+                      color: Colors.black,
+                    ),
+                    const Divider(
+                      height: 40,
+                      thickness: 5,
+                      indent: 5,
+                      color: Colors.black,
                     ),
                   ],
-                )
-              )
+                ),
+              ),
             ],
           )
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.red,
-        //  child: Icon(Icons.add_shopping_cart_sharp),
-          child: Text(
-              'A',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontFamily: 'Corinthia',
-                fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
+        )
     );
   }
 }
